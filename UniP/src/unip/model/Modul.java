c80318a9a631be11334365ac1813a84c3436e384
edra00 <elena.knoechel@iem.thm.de>
2021-03-27 00:17:16 +0100

    anstatt int note -> double note

--- UniP/src/unip/model/Modul.java
+++ UniP/src/unip/model/Modul.java
@@ -2,16 +2,16 @@ package unip.model;
 
 public class Modul {
 
-	private int modulID; // Methode?
+	private int modulID; 
 	public String modulbezeichnung;
 	public String modulname;
 	public int semester;
 	public String prüfungsform;
 	public String prüfungsvoraussetzung;
-	public int note;
+	public double note = 0d;
 	public int crp;
 	
-	public Modul (int modulID, String modulbezeichnung, String modulname, String prüfungsform, String prüfungsvoraussetzung, int note, int crp) { // woher bekommt die methode die iD?
+	public Modul (int modulID, String modulbezeichnung, String modulname, String prüfungsform, String prüfungsvoraussetzung, double note, int crp) { 
 		this.modulID = modulID;
 		this.modulbezeichnung = modulbezeichnung;
 		this.modulname = modulname;
