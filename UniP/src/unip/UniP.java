d92b6d8e5dc0cb5da72f78f19ba2d76d01d2fb7b
marcr <marcr@DESKTOP-GQRT4E8.fritz.box>
2021-03-28 18:47:13 +0200

    UniP Refactor und Multithread

--- UniP/src/unip/UniP.java
+++ UniP/src/unip/UniP.java
@@ -1,13 +1,30 @@
 package unip;
 
+import java.time.LocalTime;
+import java.util.Calendar;
+
+import unip.model.Datenmanager;
+import unip.model.Termin;
+import unip.model.Termin.Eventtype;
 import unip.view.MainController;
 
-public class Main {
+public class UniP {
 	
 	public static MainController mainController;
+	public static Datenmanager datenmanager; 
 
 	public static void main(String[] args) {
-		MainController.run(args);
+		try {
+			Thread thread = new Thread(new Runnable() {				
+				@Override
+				public void run() {
+					MainController.run(args);					
+				}
+			});
+			thread.start();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
