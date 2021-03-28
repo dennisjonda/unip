d579a5e273bc631cf51112f9dbd0b7e4a6f26bd4
edra00 <elena.knoechel@iem.thm.de>
2021-03-28 13:23:17 +0200

    todoeintrag

--- UniP/src/unip/model/ToDoEintrag.java
+++ UniP/src/unip/model/ToDoEintrag.java
@@ -2,4 +2,13 @@ package unip.model;
 
 public class ToDoEintrag {
 
+	public int NotizPosition;
+	public String Notiz;
+	public boolean Abgehakt;
+	
+	public ToDoEintrag (int NotizPosition, String Notiz, boolean Abgehakt) {
+		this.NotizPosition = NotizPosition;
+		this.Notiz = Notiz;
+		this.Abgehakt = Abgehakt; 
+	}
 }
