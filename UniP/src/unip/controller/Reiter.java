9c8b39850185b3abcda5f319b10b6e27b6ac09e9
heafun <marc.rosenberger100@gmail.com>
2021-04-24 16:59:42 +0200

    SetActive unused cleanup

--- UniP/src/unip/controller/Reiter.java
+++ UniP/src/unip/controller/Reiter.java
@@ -4,11 +4,6 @@ public abstract class Reiter {
 	private boolean active = false;
 	protected String gui;
 	
-	public void setActive(boolean active) {
-		this.active=active;
-		update();
-	}
-	
 	public abstract void update();
 	
 	public abstract void initialize();
